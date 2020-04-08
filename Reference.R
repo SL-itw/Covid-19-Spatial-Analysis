f1b9973ff37ffc386c184374afd09a1d52e08e73
sl4269 <sl4269@cumc.columbia.edu>
2020-04-08 08:33:58 -0400

    I created a reference of sources I used in the process.

---
+++ Reference.R
@@ -0,0 +1,26 @@
+
+
+
+# merging multiple datasets at a time
+# https://www.musgraveanalytics.com/blog/2018/2/12/how-to-merge-multiple-data-frames-using-base-r
+
+# creating simple dashboard
+# https://p8105.com/plotly_and_dashboards.html#flexdashboard
+
+# list of NYC zip codes
+# https://www.health.ny.gov/statistics/cancer/registry/appendix/neighborhoods.htm
+
+# guide to leaflet plot
+# https://rstudio-pubs-static.s3.amazonaws.com/508643_d6cb2a0b10484f40b1bcba052dda28e1.html
+
+# using the tigris package
+# https://journal.r-project.org/archive/2016/RJ-2016-043/RJ-2016-043.pdf
+
+# dealing with tigris errors
+# https://github.com/walkerke/tigris/issues/72
+
+# How to ownload files from github repository
+# https://stackoverflow.com/questions/14441729/read-a-csv-from-github-into-r
+
+# Covid data source
+# https://github.com/nychealth/coronavirus-data/
