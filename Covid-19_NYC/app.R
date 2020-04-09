3f4accd4abf4c1f1b2b787a84e4cb3cf83cb8e37
sl4269 <sl4269@cumc.columbia.edu>
2020-04-09 12:36:35 -0400

    added population size and case rate to dataset

--- Covid-19_NYC/app.R
+++ Covid-19_NYC/app.R
@@ -44,44 +44,67 @@ ui <- dashboardPage(
 
 server <- function(input, output) {
     
-    #plot 1
-        #data
-    # Data
-    `%notin%` = Negate(`%in%`)
+   
+   #################################### Data
+   # NYC SHAPE FILE
+    zips = c(10453, 10457, 10460,10458, 10467, 10468,10451, 10452, 10456,10454, 
+             10455, 10459, 10474,10463, 10471,10466, 10469, 10470, 10475,10461, 
+             10462,10464, 10465, 10472, 10473,11212, 11213, 11216, 11233, 11238, 
+             11209, 11214, 11228,11204, 11218, 11219, 11230,11234, 11236, 11239,
+             11223, 11224, 11229, 11235,11201, 11205, 11215, 11217, 11231,11203, 
+             11210, 11225, 11226,11207, 11208,11211, 11222,11220, 11232,11206, 
+             11221, 11237,10026, 10027, 10030, 10037, 10039,10001, 10011, 10018, 
+             10019, 10020, 10036,10029, 10035,10010, 10016, 10017, 10022,10012, 
+             10013, 10014,10004, 10005, 10006, 10007, 10038, 10280,10002, 10003, 
+             10009,10021, 10028, 10044, 10065, 10075, 10128,10023, 10024, 10025,
+             10031, 10032, 10033, 10034, 10040,11361, 11362, 11363, 11364,11354, 
+             11355, 11356, 11357, 11358, 11359, 11360,11365, 11366, 11367,11412, 
+             11423, 11432, 11433, 11434, 11435, 11436,11101, 11102, 11103, 11104, 
+             11105, 11106,11374, 11375, 11379, 11385,11691, 11692, 11693, 11694, 
+             11695, 11697,11004, 11005, 11411, 11413, 11422, 11426, 11427, 11428, 
+             11429,11414, 11415, 11416, 11417, 11418, 11419, 11420, 11421,11368, 
+             11369, 11370, 11372, 11373, 11377, 11378,10302, 10303, 10310,10306, 
+             10307, 10308, 10309, 10312,10301, 10304, 10305,10314) %>% 
+      tibble() %>% 
+      rename(zips = ".") %>% 
+      mutate(zips = as.character(zips))
     
+       nycshape <- zctas(cb = T, starts_with = c(zips$zips))
+    
+    #Census data
     covCen<- read_csv("./data/covid_census.csv")
-    #nys shape file
-    nycshape4 <- zctas(cb = T, starts_with = c("100","101","102 ","103","104","107","110","112","113", "114", "116"))
     
- 
+    
+    
     #joining file with population data
-    zipsmap <- geo_join(nycshape4, 
+    zipsmap <- geo_join(nycshape, 
                         covCen, 
                         by_sp = "GEOID10", 
                         by_df = "GEOID",
-                        how = "left")
+                        how = "inner")
+  
     
    
-    #plot
+    #plot1 population case rate
     output$plot1 <- renderLeaflet({
       #  data <- histdata[seq_len(input$slider)]
         pal <- colorNumeric(
             palette = "Blues",
-            domain = zipsmap@data$positive)
+            domain = zipsmap@data$cas)
         # create labels for zipcodes
         labels <- 
             paste0(
                 "Zip Code: ",
                 zipsmap@data$GEOID10, "<br/>",
-                "Positive Cases: ",
-                round(zipsmap@data$positive, digits = 0)) %>%
+                "Case Rate: ",
+                round(zipsmap@data$case_rate, digits = 0)) %>%
             lapply(htmltools::HTML)
         
         zipsmap %>% leaflet %>% 
             # add base map
             addProviderTiles("CartoDB") %>% 
             # add zip codes
-            addPolygons(fillColor = ~pal(positive),
+            addPolygons(fillColor = ~pal(case_rate),
                         weight = 2,
                         opacity = 1,
                         color = "white",
@@ -95,7 +118,7 @@ server <- function(input, output) {
                         label = labels) %>%
             # add legend
             addLegend(pal = pal, 
-                      values = ~positive, 
+                      values = ~case_rate, 
                       opacity = 0.7, 
                       title = htmltools::HTML("Case Rate <br/>per 100k"),
                       position = "bottomright")
